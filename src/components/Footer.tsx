be0ab1c7ad3253ad789e90d6fe3c16a9888258c6
criativismodesign <criativismodesign@users.noreply.github.com>
2026-04-07 23:49:11 -0300

    fix: remover cor duplicada do Footer e garantir espaçamento 50px correto

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -130,7 +130,7 @@ export default function Footer() {
       </div>
 
       {/* PARTE 2 - ÁREA PRINCIPAL DO FOOTER */}
-      <div style={{ height: '815px', paddingTop: '210px', padding: '210px 40px 50px 40px', backgroundColor: '#292929' }}>
+      <div style={{ height: '815px', paddingTop: '210px', padding: '210px 40px 50px 40px' }}>
         <div 
           style={{ 
             display: 'flex', 
