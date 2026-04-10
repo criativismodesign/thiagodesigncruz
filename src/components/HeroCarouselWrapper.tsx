f6c872319fa2e89d550849c4d8b14629b8787393
criativismodesign <criativismodesign@users.noreply.github.com>
2026-04-10 20:54:55 -0300

    fix: desabilitar cache no HeroCarouselWrapper

--- src/components/HeroCarouselWrapper.tsx
+++ src/components/HeroCarouselWrapper.tsx
@@ -1,9 +1,12 @@
 import { PrismaClient } from '@prisma/client'
 import HeroCarousel from './HeroCarousel'
+import { unstable_noStore as noStore } from 'next/cache'
 
 const prisma = new PrismaClient()
 
 export default async function HeroCarouselWrapper() {
+  noStore() // Desabilita cache - sempre busca dados frescos
+  
   let slides: any[] = []
   try {
     slides = await prisma.heroBanner.findMany({
