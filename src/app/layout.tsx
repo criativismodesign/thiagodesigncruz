ce97c5911a9c7df48bee02011f2e8137a44c44dc
criativismodesign <criativismodesign@users.noreply.github.com>
2026-04-11 00:48:42 -0300

    feat: adicionar favicons gerados pelo RealFaviconGenerator

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,6 +73,26 @@ export const metadata: Metadata = {
   verification: {
     google: "google-site-verification-code",
   },
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-96.png',
+    apple: '/apple-touch-icon.png',
+    other: [
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '192x192',
+        url: '/favicon-192.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '512x512',
+        url: '/favicon-512.png',
+      },
+    ],
+  },
+  manifest: '/site.webmanifest',
   category: "e-commerce",
 };
 
